001a062c058c09c30f6e046439cf7985ec09c647
Dmitriy <89617151191d@gmail.com>
2026-05-04 17:35:16 +0700

    Update AppShell.tsx

--- components/ui/AppShell.tsx
+++ components/ui/AppShell.tsx
@@ -1,25 +1,7 @@
 'use client'
 
 import { BottomNav } from './BottomNav'
-import { ChatWidget } from '@/components/chat/ChatWidget'
-import { useStore } from '@/lib/store'
 
 export function AppShell() {
-  const { items, addItem } = useStore()
-
-  return (
-    <>
-      <BottomNav />
-      <ChatWidget
-        items={items}
-        onExecuteAction={async (action: any) => {
-          if (action.type === 'add_items') {
-            for (const item of action.items) {
-              await addItem(item.name, item.category || 'products', item.quantity || 1)
-            }
-          }
-        }}
-      />
-    </>
-  )
+  return <BottomNav />
 }
