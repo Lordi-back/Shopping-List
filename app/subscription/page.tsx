de9765a7609ed3b87296f6a390dfe0b3f022f1d4
Dmitriy <89617151191d@gmail.com>
2026-05-27 17:17:48 +0700

    Update page.tsx

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,4 +1,9 @@
-import { SubscriptionPage } from '@/components/subscription/SubscriptionPage'
+import dynamic from 'next/dynamic'
+
+const SubscriptionPage = dynamic(
+  () => import('@/components/subscription/SubscriptionPage').then(mod => mod.SubscriptionPage),
+  { ssr: false }
+)
 
 export default function Page() {
   return <SubscriptionPage />
